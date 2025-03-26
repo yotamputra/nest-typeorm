e16d9eee43e1c39385eda63f4084e2542c44d326
yotamputra <yotamppy@gmail.com>
2025-03-26 11:25:52 +0700

    feat: create post register

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('/api/users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post("/register")
+  register(@Body('email') email: string, @Body('password') password: string) {
+    const user = this.userService.create();
+
+    return {
+      message: ' User created successfully',
+      data: {
+        user,
+      },
+    };
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entitiy';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create() {
+    return 'Add new user';
+  }
+
+  async findByEmail(email: string) {
+    return this.userRepository.findOne({ where: { email } });
+  }
+}
