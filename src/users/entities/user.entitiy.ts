8368cefda754340ec98cd9f977b20a291a95696a
yotamputra <yotamppy@gmail.com>
2025-03-26 10:53:23 +0700

    feat: create user entity

---
+++ src/users/entities/user.entitiy.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ unique: true}) 
+  email: string
+
+  @Column()
+  password: string
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
